749e9c36d275bc72af1278b4ec52e595da093bda
admin-project <sebastian.seidelmann.project@gmail.com>
2017-02-16 13:54:21 +0100

    [TASK] Add new exception and clean up the compiler pass

--- DependencyInjection/Compiler/AdminServiceCompilerPass.php
+++ DependencyInjection/Compiler/AdminServiceCompilerPass.php
@@ -7,6 +7,8 @@
 
 namespace AdminProject\AdminBundle\DependencyInjection\Compiler;
 
+use AdminProject\AdminBundle\Exception\Model\ModelManagerNotConfiguredForAdminService;
+use AdminProject\AdminBundle\Exception\Model\ModelManagerNotFoundException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -22,6 +24,8 @@ class AdminServiceCompilerPass implements CompilerPassInterface
     /**
      * Process the compiler pass.
      * @param ContainerBuilder $container
+     * @throws ModelManagerNotFoundException
+     * @throws ModelManagerNotConfiguredForAdminService
      * @return void
      */
     public function process(ContainerBuilder $container)
@@ -37,25 +41,43 @@ class AdminServiceCompilerPass implements CompilerPassInterface
         foreach ($taggedServices as $id => $tags) {
             $definition->addMethodCall('addAdminService', array($id, new Reference($id)));
 
-            $service = $container->getDefinition($id);
-            $service->addMethodCall('setAdminPool',             [new Reference('adminproject.admin.pool')]);
-            $service->addMethodCall('setCode',                  [$id]);
-            $service->addMethodCall('setDefaultControllerName', ['AdminProjectAdminBundle:CRUD']);
-            $service->addMethodCall('setDefaultAction',         ['list']);
+            $adminService = $container->getDefinition($id);
+            $adminService->addMethodCall('setAdminPool',             [new Reference('adminproject.admin.pool')]);
+            $adminService->addMethodCall('setCode',                  [$id]);
+            $adminService->addMethodCall('setDefaultControllerName', ['AdminProjectAdminBundle:CRUD']);
+            $adminService->addMethodCall('setDefaultAction',         ['list']);
 
             foreach ($tags as $tag) {
-                if (isset($tag['group'])) {
-                    $service->addMethodCall('addGroup', [$tag['group']]);
-                }
-                if (isset($tag['label'])) {
-                    $service->addMethodCall('setLabel', [$tag['label']]);
-                }
+                $this->applyAdminServiceTag($adminService, $tag);
 
                 if (isset($tag['manager_type'])) {
-                    $this->applyModelManager($container, $service, $tag['manager_type']);
+                    $this->applyModelManager($container, $adminService, $tag['manager_type']);
                 } else {
-                    throw new \Exception('manager_type must be given for service ' . $id);
+                    throw new ModelManagerNotConfiguredForAdminService(sprintf(
+                        'manager_type must be given for service "%s"',
+                        $id
+                    ));
                 }
+            }
+        }
+    }
+
+    /**
+     * Apply the tags for the admin service.
+     * @param Definition $adminService
+     * @param array      $tag
+     * @return void
+     */
+    private function applyAdminServiceTag(Definition $adminService, array $tag)
+    {
+        $tagMapping = [
+            'group' => 'addGroup',
+            'label' => 'setLabel'
+        ];
+
+        foreach ($tagMapping as $tagName => $methodName) {
+            if (isset($tag[$tagName])) {
+                $adminService->addMethodCall($methodName, [$tag[$tagName]]);
             }
         }
     }
@@ -63,19 +85,45 @@ class AdminServiceCompilerPass implements CompilerPassInterface
     /**
      * Apply the model manager.
      * @param ContainerBuilder $container
-     * @param Definition       $service
+     * @param Definition       $adminService
      * @param string           $managerType
-     * @throws \Exception
+     * @return void
+     * @throws ModelManagerNotFoundException
      */
-    private function applyModelManager(ContainerBuilder $container, Definition $service, $managerType)
+    private function applyModelManager(ContainerBuilder $container, Definition $adminService, $managerType)
     {
         $managerTypeServiceId = sprintf('adminproject.manager.%s', $managerType);
+
         if (!$container->has($managerTypeServiceId)) {
-            throw new \Exception('Model Manager ' . $managerTypeServiceId . ' not found');
+            throw new ModelManagerNotFoundException('Model Manager ' . $managerTypeServiceId . ' not found');
         }
 
-        $service->addMethodCall('setModelManager',       [new Reference($managerTypeServiceId)]);
-        $service->addMethodCall('setDatagridBuilder',    [new Reference(sprintf('adminproject.builder.%s_datagrid', $managerType))]);
-        $service->addMethodCall('setFieldmapperBuilder', [new Reference(sprintf('adminproject.builder.%s_fieldmapper', $managerType))]);
+        $adminService->addMethodCall(
+            'setModelManager',
+            [
+                new Reference($managerTypeServiceId)
+            ]
+        );
+
+        foreach ($this->getModelManagerServicesMapping() as $serviceName => $method) {
+            $adminService->addMethodCall(
+                $method,
+                [
+                    new Reference(sprintf('adminproject.builder.%s_%s', $managerType, $serviceName))
+                ]
+            );
+        }
+    }
+
+    /**
+     * Returns the additional model manager services mapping to inject in admin service.
+     * @return array
+     */
+    private function getModelManagerServicesMapping()
+    {
+        return [
+            'datagrid' => 'setDatagridBuilder',
+            'fieldmapper' => 'setFieldmapperBuilder'
+        ];
     }
 }
---
+++ Exception/Model/ModelManagerNotConfiguredForAdminService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class ModelManagerNotConfiguredForAdminService
+ * @package AdminProject\AdminBundle\Exception\Model
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+
+namespace AdminProject\AdminBundle\Exception\Model;
+
+use AdminProject\AdminBundle\Exception\AdminBundleException;
+
+/**
+ * Class ModelManagerNotConfiguredForAdminService
+ * @package AdminProject\AdminBundle\Exception\Model
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+class ModelManagerNotConfiguredForAdminService extends AdminBundleException
+{
+
+}
